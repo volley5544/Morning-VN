3a64a3d359dae2a3fa0dcab348eb131a6bd6da85
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-10 11:28:04 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -3,3 +3,4 @@ export 'periodic_get_location.dart' show periodicGetLocation;
 export 'check_firebase.dart' show checkFirebase;
 export 'login.dart' show login;
 export 'check_user_firebase.dart' show checkUserFirebase;
+export 'background_location_permission.dart' show backgroundLocationPermission;
